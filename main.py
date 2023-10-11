78dc80a10b186069c2d7ec575600a58f113e25a1
Th3Prim3 <45297253+Th3Prim3@users.noreply.github.com>
2023-10-11 12:27:45 -0500

    Cleaned data and created graphs.

--- main.py
+++ main.py
@@ -8,7 +8,8 @@
 # (5/5 points) Proper import of packages used.
 import pandas as pd
 from sodapy import Socrata
-from configs import APIToken, cdcPWD, myUsername
+#from configs import APIToken, cdcPWD, myUsername
+import matplotlib.pyplot as plt
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
@@ -19,24 +20,47 @@ from configs import APIToken, cdcPWD, myUsername
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("nt65-c7a7", limit=2000)
-pd.read_csv(NCHSMort)
+#results = client.get("nt65-c7a7", limit=2000)
+results_df = pd.read_csv("NCHSMortality.csv")
+
 
 # (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data
 # is labeled tabular data.
 # Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
+#results_df = pd.DataFrame.from_records(results)
 
-#injuryMechDeath = results_df[['injury_mechanism', 'deaths']].where(results_df['injury_mechanism'] == "Firearm")
-#print(injuryMechDeath)
 
-n = []
+# Filter csv to just results that match the criteria below
+injuryMechDeath = results_df[(results_df["Injury mechanism"] == "Firearm") &
+                             (results_df["Race"] == "All races") &
+                             (results_df["Age group (years)"] == "All Ages") &
+                             (results_df["Sex"] == "Both sexes") &
+                             (results_df["Injury intent"] == "All Intentions")]
+
+# Only strip to years and deaths
+columns = ['Year', 'Deaths']
+cleaned_injuryMechDeath = injuryMechDeath[columns]
+
+
+x_axis = cleaned_injuryMechDeath["Year"].values.tolist()
+y_axis = cleaned_injuryMechDeath["Deaths"].values.tolist()
+
+plt.plot(x_axis, y_axis)
+plt.xlabel("Years")
+plt.ylabel("Deaths")
+plt.axis((1999, 2016, 20000, 50000))
+plt.show()
+
+
+
+
+"""
 
 for result in results_df:
     print(result)
     #if result["injury_mechanism"] == "Firearm":
        # n.append(result)
-
+"""
 
 
 
@@ -50,6 +74,11 @@ for result in results_df:
 
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build
 # some fancy charts here as it will greatly help you in future homework assignments and in the final project.
+
+
+
+
+
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder,
 # the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
